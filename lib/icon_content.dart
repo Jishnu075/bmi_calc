e8dbbb87ec1bb80eb81c166bbb4d53c39bdbefa1
Jishnu075 <jishnubiju075@gmail.com>
2021-09-19 22:40:44 +0530

    3rd card

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-
-const labelTextStyle = TextStyle(
-  color: Color(0xFF8D8E98),
-  fontSize: 18.0,
-);
+import 'constants.dart';
 
 class IconContent extends StatelessWidget {
   final String label;
@@ -23,7 +19,7 @@ class IconContent extends StatelessWidget {
         SizedBox(
           height: 15.0,
         ),
-        Text(label, style: labelTextStyle),
+        Text(label, style: kLabelTextStyle),
       ],
     );
   }
